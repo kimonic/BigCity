bc348f07bac195721f803c80a7cdc82f9b923019
Administrator <2031382182@qq.com>
2017-09-14 16:05:37 +0800

    20170913

--- app/src/main/java/com/bigcity/bombtest/BombTestActivity.java
+++ app/src/main/java/com/bigcity/bombtest/BombTestActivity.java
@@ -9,11 +9,14 @@ import com.bigcity.R;
 import com.bigcity.base.BaseActivity;
 import com.bigcity.utils.ToastUtils;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.bmob.v3.BmobObject;
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.QueryListener;
 import cn.bmob.v3.listener.SaveListener;
 import cn.bmob.v3.listener.UpdateListener;
@@ -70,16 +73,44 @@ public class BombTestActivity extends BaseActivity {
                 break;
             case R.id.bt_act_bombtest2://查询数据
 
-                //查找Person表里面id为6b6c11c537的数据
-                BmobQuery<Person> bmobQuery = new BmobQuery<Person>();
-                bmobQuery.getObject("4baaee125b", new QueryListener<Person>() {
+//                //查找Person表里面id为6b6c11c537的数据
+//                BmobQuery<Person> bmobQuery = new BmobQuery<Person>();
+//                bmobQuery.getObject("4baaee125b", new QueryListener<Person>() {
+//                    @Override
+//                    public void done(Person object, BmobException e) {
+//                        if (e == null) {
+//                            ToastUtils.showToast(BombTestActivity.this, "查询成功");
+//                            tvContent.setText(object.getAddress() + "\n" + object.getName());
+//                        } else {
+//                            ToastUtils.showToast(BombTestActivity.this, "查询失败：" + e.getMessage());
+//                        }
+//                    }
+//                });
+
+
+                BmobQuery<Person> query = new BmobQuery<Person>();
+                //查询playerName叫“比目”的数据
+                query.addWhereEqualTo("name", "lucky");
+                //返回50条数据，如果不加上这条语句，默认返回10条数据
+                query.setLimit(50);
+                //执行查询方法
+                query.findObjects(new FindListener<Person>() {
                     @Override
-                    public void done(Person object, BmobException e) {
+                    public void done(List<Person> object, BmobException e) {
                         if (e == null) {
-                            ToastUtils.showToast(BombTestActivity.this, "查询成功");
-                            tvContent.setText(object.getAddress() + "\n" + object.getName());
+                            ToastUtils.showToast(BombTestActivity.this, "查询成功：共" + object.size() + "条数据。");
+                            StringBuilder builder = new StringBuilder();
+                            for (Person Person : object) {
+                                builder.append(Person.getName())
+                                        .append("------")
+                                        .append(Person.getObjectId())
+                                        .append("------")
+                                        .append(Person.getCreatedAt())
+                                        .append("\n");
+                            }
+                            tvContent.setText(builder.toString());
                         } else {
-                            ToastUtils.showToast(BombTestActivity.this, "查询失败：" + e.getMessage());
+                            ToastUtils.showToast(BombTestActivity.this, "失败：" + e.getMessage() + "," + e.getErrorCode());
                         }
                     }
                 });
@@ -92,10 +123,10 @@ public class BombTestActivity extends BaseActivity {
 
                     @Override
                     public void done(BmobException e) {
-                        if(e==null){
-                            ToastUtils.showToast(BombTestActivity.this, "更新成功:"+p4.getUpdatedAt());
-                        }else{
-                            ToastUtils.showToast(BombTestActivity.this,"更新失败：" + e.getMessage());
+                        if (e == null) {
+                            ToastUtils.showToast(BombTestActivity.this, "更新成功:" + p4.getUpdatedAt());
+                        } else {
+                            ToastUtils.showToast(BombTestActivity.this, "更新失败：" + e.getMessage());
                         }
                     }
 
@@ -107,10 +138,10 @@ public class BombTestActivity extends BaseActivity {
                 p3.delete(new UpdateListener() {
                     @Override
                     public void done(BmobException e) {
-                        if(e==null){
-                            ToastUtils.showToast(BombTestActivity.this,"删除成功:"+p3.getUpdatedAt());
-                        }else{
-                            ToastUtils.showToast(BombTestActivity.this,"删除失败：" + e.getMessage());
+                        if (e == null) {
+                            ToastUtils.showToast(BombTestActivity.this, "删除成功:" + p3.getUpdatedAt());
+                        } else {
+                            ToastUtils.showToast(BombTestActivity.this, "删除失败：" + e.getMessage());
                         }
                     }
 
